f1b13d53d7192dcb3008ea6173b701c27a551f8a
sachin poojary <75829154+sachinpoojary1997@users.noreply.github.com>
2023-04-29 21:26:12 +0530

    Add files via upload

---
+++ sachin_protfolio/cv.js
@@ -0,0 +1,29 @@
+
+//======about me========//
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add('active-tab');
+}
+
+
+// ====about sidemenu==== //
+var sidemenu = document.getElementById('sidemenu');
+function openmenu(){
+    sidemenu.style.right = '0';
+}
+function closemenu(){
+    sidemenu.style.right = '-150px';
+}
+
+
+// ====== google form code ====== //
+
+//this is not done at//
